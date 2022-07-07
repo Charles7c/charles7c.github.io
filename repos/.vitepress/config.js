eae3073987eba4a47170bd4b80c53909fed6d9d9
Charles7c <charles7c@126.com>
2022-07-07 23:10:35 +0800

    修改部分配置

--- repos/.vitepress/config.js
+++ repos/.vitepress/config.js
@@ -1,38 +1,37 @@
-export default ({
+export default {
   lang: 'zh-CN',
   title: '查尔斯的知识库',
   description: '个人知识库，记录 & 分享个人碎片化、结构化、体系化的知识内容。',
-
-  lastUpdated: true, // 启用最后更新时间
 
   // <head>设置
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
 
+  lastUpdated: true, // 启用最后更新时间
+
   themeConfig: {
     logo: '/logo.jpg',
-    lastUpdatedText: '最后更新时间', // 最后更新时间文本
-    docFooter: {
-      prev: '上一页',
-      next: '下一页'
-    },
+    outlineTitle: '本页内容', // 右侧边栏的大纲标题
     // 编辑链接
     editLink: {
       pattern: 'https://github.com/Charles7c/charles7c.github.io/edit/main/repos/:path',
       text: '在 GitHub 上编辑此页面'
     },
-
-    // 社交链接
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/Charles7c/charles7c.github.io' }
-    ],
-
+    lastUpdatedText: '最后更新', // 最后更新时间文本
+    docFooter:{
+      prev: '上一页',
+      next: '下一页'
+    },
     // 版权标识
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2019-present Charles7c'
     },
+    // 社交链接
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Charles7c/charles7c.github.io' }
+    ],
 
     // 导航栏
     nav: nav(),
@@ -44,7 +43,7 @@ export default ({
      '/about/': sidebarAbout()
     }
   }
-})
+}
 
 function nav() {
   return [
@@ -53,7 +52,7 @@ function nav() {
       link: '/issues/index'
     },
     {
-      text: '杂碎逆袭史',
+      text: '"杂碎"逆袭史',
       link: '/fragments/index'
     },
     {
