ee8cfef365f82d4733c84005e207507b0f2ad205
Charles7c <charles7c@126.com>
2022-08-16 10:01:59 +0800

    修复：代码字体被normalize.css污染的问题

--- docs/.vitepress/config/markdown.ts
+++ docs/.vitepress/config/markdown.ts
@@ -1,7 +1,11 @@
 import type { MarkdownOptions } from 'vitepress'
 
 export const markdown: MarkdownOptions = {
-  theme: 'one-dark-pro', // Shiki主题, 所有主题参见: https://github.com/shikijs/shiki/blob/main/docs/themes.md
+  // Shiki主题, 所有主题参见: https://github.com/shikijs/shiki/blob/main/docs/themes.md
+  theme: {
+    light: 'material-palenight',
+    dark: 'one-dark-pro'
+  },
   lineNumbers: true, // 启用行号
 
   // 在所有文档的<h1>标签后添加<ArticleMetadata/>组件
