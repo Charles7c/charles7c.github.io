0b24b3b81e3f3ae467e741a8f1f14d5b4d008e34
Charles7c <charles7c@126.com>
2022-08-19 21:13:35 +0800

    新增：《Java基础快速入门 | Java概述》

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -13,6 +13,7 @@ export const nav: DefaultTheme.Config['nav'] = [
   {
     text: '我的小册',
     items: [
+      { text: 'Java基础快速入门', link: '/courses/java/index', activeMatch: '/courses/java/' },
       { text: 'MyBatis快速入门', link: '/courses/mybatis/index', activeMatch: '/courses/mybatis/' }
     ],
     activeMatch: '/courses/'
