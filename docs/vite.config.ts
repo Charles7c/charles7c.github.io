f7781ac3289ece4e9d3201f446bfc6991504d968
Charles7c <charles7c@126.com>
2023-03-29 21:23:25 +0800

    优化：使用 vitepress-plugin-pagefind 离线搜索插件替换 vitepress-plugin-search 插件，中文搜索相对友好一些（毕竟国人写的）

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import Components from 'unplugin-vue-components/vite';
 import { ArcoResolver } from 'unplugin-vue-components/resolvers';
-// import { SearchPlugin } from 'vitepress-plugin-search';
+// import { pagefindPlugin } from 'vitepress-plugin-pagefind';
 
 export default defineConfig({
   plugins: [
-    // SearchPlugin({
-    //   encode: false,
-    //   tokenize: 'full',
+    // pagefindPlugin({
+      // btnPlaceholder: '搜索',
+      // placeholder: '搜索文档',
+      // emptyText: '空空如也',
+      // heading: '共 {{searchResult}} 条结果'
     // }),
     Components({
       dirs: ['.vitepress/theme/components'],
