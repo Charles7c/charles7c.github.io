d99fa66aae1f17f7e1fc09fccc89ee6dd2b47488
Charles7c <charles7c@126.com>
2023-08-02 22:28:46 +0800

    优化：优化文章序号及部分图标样式

--- docs/.vitepress/theme/utils.ts
+++ docs/.vitepress/theme/utils.ts
@@ -37,3 +37,23 @@ export function goToLink(url, paramName, paramValue) {
     window.location.href = url;
   }
 }
+
+/**
+ * 获取生肖图标
+ *
+ * @param year 年份
+ */
+export function getChineseZodiac(year) {
+  const arr = ['monkey', 'rooster', 'dog', 'pig', 'rat', 'ox', 'tiger', 'rabbit', 'dragon', 'snake', 'horse', 'goat'];
+  return arr[year % 12];
+}
+
+/**
+ * 获取生肖名称
+ *
+ * @param year 年份
+ */
+export function getChineseZodiacAlias(year) {
+  const arr = ['猴年', '鸡年', '狗年', '猪年', '鼠年', '牛年', '虎年', '兔年', '龙年', '蛇年', '马年', '羊年'];
+  return arr[year % 12];
+}
